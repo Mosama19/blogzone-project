5573bb9abf903a52edf27abfbf7e556658adac07
Osama malik <oosamamalik12@gmail.com>
2023-05-29 10:03:06 -0700

    Blog data

--- src/components/blog/EssentiaRead.js
+++ src/components/blog/EssentiaRead.js
@@ -1,25 +1,23 @@
+import Box from "../shared/Box"
 import blogimg1 from "./images/blog1.jpg"
+import blogData from "./blogData"
 const EssentialRead = () => {
     return (
         <div className="w-full">
-            <section className="w-5/6 md:w-4/6 mx-auto">
+            <section className="w-10/12 md:w-5/6 mx-auto">
                 <div className="my-20">
                     <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl">Discover the best of the web</h1>
                     <h4 className="font-bold text-xl sm:text-2xl mt-2">Today’s essential reads</h4>
                 </div>
                 <div>
-                    <div>
-                        <div className="w-96 h-96 bg-white shadow-sm border-2 rounded-2xl">
-                            <img src={blogimg1} alt="blog1" className="rounded-tr-lg rounded-tl-lg"/>
-                            <div className="mx-3">
-                                <h1 className="font-semibold mt-4">Down the Rabbit Hole: Why People Fall for Conspiracy Theories</h1>
-                                <p className="mt-2">And how to talk to those who have fallen for falsehoods.</p>
-                            </div>
-                            <div className="mx-3 mt-2 flex justify-between font-medium">
-                                <div>News Literacy Project</div>
-                                <div>Save</div>
-                            </div>
-                        </div>
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 my-20">
+                        {
+                            blogData.map((item) => {
+                                return (
+                                    <Box src={item.img} title={item.title} desc={item.desc} source={item.source} link={item.link} />
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </section>
---
+++ src/components/shared/Box.js
@@ -0,0 +1,27 @@
+import {CiBookmarkPlus} from 'react-icons/ci'
+const Box = (props) => {
+    const { src,title,desc,source,link } = props;
+    return (
+        <a href={link} target='_blank'>
+        <div className="w-full bg-white shadow-sm my-2 border-2 rounded-2xl  md:hover:shadow-2xl cursor-pointer">
+            <div className="flex flex-row md:flex-col">
+                <img src={src} alt="blog1" className="rounded-xl w-4/12 h-3/12 m-2 md:m-0 md:w-full md:rounded-none md:rounded-t-xl" />
+                <div>
+                    <div className="mx-3">
+                        <h1 className="font-semibold mt-4">{title}</h1>
+                        <p className="mt-2">{desc}</p>
+                    </div>
+                    <div className="mx-3 mt-2 flex justify-between font-medium md:mb-2">
+                        <div>{source}</div>
+                        <div className='flex cursor-pointer'>
+                            <CiBookmarkPlus size={25}/>
+                            <div>Save</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </a>
+    )
+}
+export default Box
